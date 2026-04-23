a0794fbbb18001a364cf7a75a53720d165a39ee4
Dishitha0607 <dishi0607@gmail.com>
2026-04-23 18:09:45 +0530

    Added Home Page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import { getProductById, getProducts } from "../data/products";
+import ProductCard from "../components/ProductCard";
+
+function Home() {
+  // Getting the products from products.js
+  const products = getProducts();
+
+  return (
+    <>
+      <div className="page">
+        <div className="home-hero">
+          <h1 className="home-title">Welcome to ShopHub</h1>
+          <p className="home-subtitle">
+            Discover amazig products at greate prices
+          </p>
+        </div>
+
+        <div className="container">
+          <h2 className="page-title">Our Products</h2>
+          <div className="product-grid">
+            {/* Looping through each product */}
+            {products.map((product) => {
+              return <ProductCard product={product} key={product.id}/>;
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
